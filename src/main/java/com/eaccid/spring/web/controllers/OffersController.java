c299a33302c6dcdebf6106e7caa0951b1c14aa2a
EAccid <elena.briginets@gmail.com>
2016-09-27 20:50:33 +0300

    Adding data to the session

--- src/main/java/com/eaccid/spring/web/controllers/OffersController.java
+++ src/main/java/com/eaccid/spring/web/controllers/OffersController.java
@@ -1,19 +1,38 @@
 package com.eaccid.spring.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class OffersController {
 
     @RequestMapping("/")
-    public String showHome(HttpSession session) {
-
-        session.setAttribute("name", "Boris");
-
+    public String showHome(Model model) {
+        model.addAttribute("name", "<b>Tiffany</b>");
         return "home";
     }
+
+   /*
+    public ModelAndView showHome() {
+        ModelAndView mv = new ModelAndView("home");
+        Map<String, Object> model = mv.getModel();
+        model.put("name", "<b>River</b>");
+        return mv;
+    }
+
+    public String showHome(HttpSession session) {
+        session.setAttribute("name", "Boris");
+        return "home";
+    }
+
+
+    public String showHome(Model model) {
+        model.addAttribute("name", "Tiffany");
+        return "home";
+    }
+*/
+
 
 }
--- src/main/webapp/WEB-INF/jsps/home.jsp
+++ src/main/webapp/WEB-INF/jsps/home.jsp
@@ -1,20 +1,19 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: AA
-  Date: 27.09.2016
-  Time: 13:25
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <title>home.jsp</title>
 </head>
 <body>
 
-Hi there!
+Hi there! <p/>
 
-Session: <%= session.getAttribute("name") %> <p/>
+lesson 56 - Session: <%= session.getAttribute("name") %> <p/>
+lesson 57 - Request: <%= request.getAttribute("name") %> <p/>
+
+lesson 57 - Request (using EL): ${name} <p/>
+<c:out value="${name}">|</c:out> <p/>
 
 </body>
 </html>
