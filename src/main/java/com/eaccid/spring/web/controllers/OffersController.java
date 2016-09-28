21067a2fbc92eb08d8d0292f578e758f02df73af
EAccid <elena.briginets@gmail.com>
2016-09-28 11:52:36 +0300

    Creating a Datasource Bean

--- src/main/java/com/eaccid/spring/web/controllers/OffersController.java
+++ src/main/java/com/eaccid/spring/web/controllers/OffersController.java
@@ -8,32 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OffersController {
 
+    //arguments described in home.xml
+
     @RequestMapping("/")
     public String showHome(Model model) {
         model.addAttribute("nameBold", "<b>Tiffany</b>");
         model.addAttribute("name", "<b>Tiffany</b>");
         return "home";
     }
-
-   /*
-    public ModelAndView showHome() {
-        ModelAndView mv = new ModelAndView("home");
-        Map<String, Object> model = mv.getModel();
-        model.put("name", "<b>River</b>");
-        return mv;
-    }
-
-    public String showHome(HttpSession session) {
-        session.setAttribute("name", "Boris");
-        return "home";
-    }
-
-
-    public String showHome(Model model) {
-        model.addAttribute("name", "Tiffany");
-        return "home";
-    }
-*/
-
 
 }
