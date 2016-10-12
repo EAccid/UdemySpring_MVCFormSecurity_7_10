cca3f925d45d5c4fedb94ff76646dc24f566a607
EAccid <elena.briginets@gmail.com>
2016-10-12 19:24:19 +0300

    130. Getting the Username of the Logged-In User

--- src/main/java/com/eaccid/spring/web/dao/OffersDAO.java
+++ src/main/java/com/eaccid/spring/web/dao/OffersDAO.java
@@ -25,7 +25,8 @@ public class OffersDao {
     }
 
     public List<Offer> getOffers() {
-        return jdbc.query("select * from offers", (rs, rowNum) -> {
+        return jdbc.query("select * from offers, users where offers.username = users.username " +
+                "and enabled = true", (rs, rowNum) -> {
             return newOffer(rs);
         });
     }
@@ -33,14 +34,14 @@ public class OffersDao {
     public boolean update(Offer offer) {
         BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
 
-        return jdbc.update("update offers set name=:name, text=:text, email=:email where id=:id", params) == 1;
+        return jdbc.update("update offers set text=:text where id=:id", params) == 1;
     }
 
     public boolean create(Offer offer) {
 
         BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
 
-        return jdbc.update("insert into offers (name, text, email) values (:name, :text, :email)", params) == 1;
+        return jdbc.update("insert into offers (username, text) values (:username, :text)", params) == 1;
     }
 
     @Transactional
@@ -48,7 +49,7 @@ public class OffersDao {
 
         SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
 
-        return jdbc.batchUpdate("insert into offers (id, name, text, email) values (:id, :name, :text, :email)", params);
+        return jdbc.batchUpdate("insert into offers (username, text) values (:username, :text)", params);
     }
 
     public boolean delete(int id) {
@@ -62,18 +63,28 @@ public class OffersDao {
         MapSqlParameterSource params = new MapSqlParameterSource();
         params.addValue("id", id);
 
-        return jdbc.queryForObject("select * from offers where id=:id", params, (rs, rowNum) -> {
+        return jdbc.queryForObject("select * from offers, users where offers.username = users.username " +
+                "and enabled = true", params, (rs, rowNum) -> {
             return newOffer(rs);
         });
     }
 
     private Offer newOffer(ResultSet rs) throws SQLException {
+
+        User user = new User();
+        user.setAuthority(rs.getString("authority"));
+        user.setEmail(rs.getString("email"));
+        user.setEnabled(true);
+        user.setName(rs.getString("name"));
+        user.setUsername(rs.getString("username"));
+
+
         Offer offer = new Offer();
         offer.setId(rs.getInt("id"));
-        offer.setName(rs.getString("name"));
         offer.setText(rs.getString("text"));
-        offer.setEmail(rs.getString("email"));
+        offer.setUser(user);
         return offer;
+
     }
 
 }
